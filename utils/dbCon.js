f3930caa433bd1cb997eab6a80bc2a945292d945
dshelton2001 <de690776@ucf.edu>
2024-02-21 03:02:59 -0500

    Added .env capabilities and created connection util

---
+++ utils/dbCon.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+// so we can get extra data if we're running it locally from our git
+const isDebug = process.env.NODE_ENV === 'production';
+
+const host = process.env.DB_URL;
+const port = process.env.DB_PORT;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const database = process.env.DB_NAME;
+
+var dbCon = mysql.createPool({
+	connectionLimit: 100,
+	host,
+	port,
+	user,
+	password,
+	database,
+	debug: isDebug
+});
+
+module.exports = dbCon;
